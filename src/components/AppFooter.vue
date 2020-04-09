7ecc7befb611b239293b8ddb37e04705e76eb728
Robin Simonklein <rsimonklein@gmail.com>
2020-04-09 15:38:42 +0200

    Add contribute in footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,6 +4,7 @@
             <p v-html="$t('site_data_message')"></p>
         </div>
         <div class="footer__links">
+            {{ $t('contribute') }} :
             <a class="footer__link" href="https://github.com/robinsimonklein/confined" target="_blank">
                 <img class="footer__link-img" src="@/assets/svg/github.svg">
             </a>
@@ -25,6 +26,11 @@
         align-items: flex-end;
         width: 100%;
         padding: 1rem;
+        margin-bottom: 1rem;
+
+        @media screen and (max-width: 548px) {
+            flex-direction: column;
+        }
 
         &__infos {
             text-align: left;
@@ -35,6 +41,8 @@
 
             @media screen and (max-width: 548px) {
                 max-width: inherit;
+                margin-right: 0;
+                margin-bottom: 2rem;
             }
 
             p {
@@ -45,10 +53,18 @@
         &__links {
             display: flex;
             justify-content: flex-end;
+            align-items: center;
+            text-transform: capitalize;
+
+            @media screen and (max-width: 548px) {
+                width: 100%;
+                justify-content: start;
+            }
         }
 
         &__link {
             font-size: 0;
+            margin-left: .5rem;
         }
         &__link-img {
             height: 2rem;
