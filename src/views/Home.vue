c4316001ccc5e555681eb0457536a7ad4cc0be7a
Robin Simonklein <rsimonklein@gmail.com>
2020-04-14 12:15:32 +0200

    Add Africa countries

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -59,6 +59,10 @@
                                 date: "2020-03-17",
                                 time: "12:00"
                             },
+                            estimatedEnd:  {
+                                date: "2020-05-11",
+                                time: "12:00"
+                            },
                             timezone: "Europe/Paris",
                             source: 'https://fr.wikipedia.org/wiki/Confinement_de_2020_en_France'
                         },
@@ -270,6 +274,82 @@
                             flag: 'venezuela',
                             beginning: {
                                 date: "2020-03-17",
+                                time: "00:00"
+                            },
+                        },
+                    ],
+                    africa: [
+                        {
+                            flag: 'morocco',
+                            beginning: {
+                                date: "2020-03-20",
+                                time: "00:00"
+                            },
+                            estimatedEnd: {
+                                date: "2020-04-20",
+                                time: "00:00"
+                            }
+                        },
+                        {
+                            flag: 'tunisia',
+                            beginning: {
+                                date: "2020-03-22",
+                                time: "00:00"
+                            },
+                        },
+                        {
+                            flag: 'algeria',
+                            beginning: {
+                                date: "2020-03-24",
+                                time: "00:00"
+                            },
+                            source: "https://www.jeuneafrique.com/925469/societe/maroc-tunisie-algerie-le-confinement-a-lepreuve-de-la-realite-sociale/"
+                        },
+                        {
+                            flag: 'zimbabwe',
+                            beginning: {
+                                date: "2020-03-30",
+                                time: "00:00"
+                            },
+                        },
+                        {
+                            flag: 'botswana',
+                            beginning: {
+                                date: "2020-04-02",
+                                time: "00:00"
+                            },
+                            estimatedEnd: {
+                                date: "2020-04-30",
+                                time: "00:00"
+                            },
+                            source: "https://www.iol.co.za/news/africa/coronavirus-in-africa-botswanas-president-to-seek-6-month-extension-of-state-of-emergency-46349571"
+                        },
+                        {
+                            flag: 'nigeria',
+                            beginning: {
+                                date: "2020-03-30",
+                                time: "00:00"
+                            },
+                            estimatedEnd: {
+                                date: "2020-04-26",
+                                time: "00:00"
+                            },
+                        },
+                        {
+                            flag: 'rwanda',
+                            beginning: {
+                                date: "2020-03-22",
+                                time: "00:00"
+                            },
+                            estimatedEnd: {
+                                date: "2020-04-19",
+                                time: "00:00"
+                            },
+                        },
+                        {
+                            flag: 'south_africa',
+                            beginning: {
+                                date: "2020-03-27",
                                 time: "00:00"
                             },
                         },
