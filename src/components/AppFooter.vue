d07c6d6824395462d3f41b38935fb33a4a026f9d
Robin Simonklein <rsimonklein@gmail.com>
2020-04-18 01:26:09 +0200

    wording

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,7 +2,7 @@
     <footer class="footer">
         <div class="footer__infos">
             <p v-html="$t('site_data_message')"></p>
-            <p id="asterisk-1">* {{ $t('asterisk_1') }}</p>
+            <p id="asterisk-1"><sup>1</sup> {{ $t('asterisk_1') }}</p>
         </div>
         <div class="footer__links">
             {{ $t('contribute') }} :
